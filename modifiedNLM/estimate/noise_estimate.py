eea3de1bf73718c711d942d65db1992a803879f0
Gustavo Solcia <gustavo.solcia@usp.br>
2022-03-17 08:19:14 -0300

    Update noise_estimate documentation

--- modifiedNLM/estimate/noise_estimate.py
+++ modifiedNLM/estimate/noise_estimate.py
@@ -1,7 +1,7 @@
 # Author: Gustavo Solcia
 # E-mail: gustavo.solcia@usp.br
 
-"""Standard deviation estimate based on the paper: Residual variance and residual pattern in nonlinear regression BY THEO GASSER.
+"""Standard deviation estimate inspired on the paper: Residual variance and residual pattern in nonlinear regression BY THEO GASSER.
 
 """
 
@@ -13,14 +13,20 @@ def rician_estimate(img):
     
     Parameters
     ----------
-        img: array
+    img: array
+        Input nd array from image that you want to estimate the standard deviation.
 
     Return
     ------
+    sigma_est: float
+        Estimated standard deviation inspired on Theo Gasser equations.
 
     References
     ----------
-        Wiest-Daesslé N, Prima S, Coupé P, Morrissey SP, Barillot C. Rician noise removal by non-Local Means filtering for low signal-to-noise ratio MRI: applications to DT-MRI. Med Image Comput Comput Assist Interv. 2008;11(Pt 2):171-9. doi: 10.1007/978-3-540-85990-1_21. PMID: 18982603; PMCID: PMC2665702.
+        Wiest-Daesslé N, Prima S, Coupé P, Morrissey SP, Barillot C. Rician noise removal 
+        by non-Local Means filtering for low signal-to-noise ratio MRI: applications to 
+        DT-MRI. Med Image Comput Comput Assist Interv. 2008;11(Pt 2):171-9.
+        doi: 10.1007/978-3-540-85990-1_21. PMID: 18982603; PMCID: PMC2665702.
 
     """
 
@@ -35,5 +41,5 @@ def rician_estimate(img):
         c = 1 / (a * a + b * b + 1)        
         
         sigma += c * c * (a * X[i-1] + b * X[i+1] - X[i]) * (a * X[i-1] + b * X[i+1] - X[i])
-
-    return np.sqrt(sigma / size)
+    sigma_est = np.sqrt(sigma / size)
+    return sigma_est
