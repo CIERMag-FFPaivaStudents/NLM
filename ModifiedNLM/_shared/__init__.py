63afb9ff8090a4b075f9f7fe806700338bc5d213
Rafael Henrique <rafaelhenri@usp.br>
2022-03-31 22:55:09 -0300

    Removing Directory

--- ModifiedNLM/_shared/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Shared code from scikit-image that is necessary for Non-Local means."""
-
-__version__ = '0.1.0'
-__author__ = 'Gustavo Solcia <gustavo.solcia@usp.br>'
-__all__ = []
--- ModifiedNLM/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Python code for testing modifiedNLM module"""
-
-from .test_sample import test_pass
-
-__version__ = '0.1.0'
-__author__ = 'Rafael Henrique <rafaelhenri@usp.br>'
-__all__ = ['test_pass']
